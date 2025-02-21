5246c0c3de8875d1ee92201fc9764d988cac47a5
Nurmuhammad-x <130286789+Nurmuhammad-Xusanov@users.noreply.github.com>
2025-02-21 23:40:55 +0500

    food show update

--- resources/views/admin/foods/show.blade.php
+++ resources/views/admin/foods/show.blade.php
@@ -10,58 +10,41 @@
     <div class="py-10">
         <div class="max-w-7xl mx-auto px-6 lg:px-8 space-y-5 bg-gray-800 rounded py-2">
 
+            <div class="relative min-h-52 p-4">
+                <a data-fancybox="gallery" href="{{$food->image}}">
+                    <img class="sm:max-h-40 md:max-h-44 max-h-40 rounded-lg float-left mr-4 mb-2" src="{{$food->image}}"
+                        alt="{{$food->title}}">
+                </a>
+
+                <h1 class="text-white text-lg font-bold">{{$food->title}}</h1>
+                <p class="text-white">{{$food->description}}</p>
+            </div>
+
             <div
-                class="flex flex-col lg:flex-row items-stretch lg:items-start space-y-4 lg:space-y-0 lg:space-x-4">
-                <div>
-                    <img class="max-h-72 rounded " src="{{$food->image}}" alt="{{$food->title}}">
-                </div>
-                <!-- Description Section -->
-                <div class="flex flex-col gap-2 w-full lg:w-2/3">
-                    <h1 class="text-white text-lg font-bold text-center lg:text-left">{{$food->title}}</h1>
-                    <div class="flex-1 overflow-y-auto">
-                        
+                class="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-full max-w-7xl mx-auto px-6 lg:px-8 bg-gray-800">
+                <div class="w-full pt-2 flex justify-between items-center">
+                    <div class="flex flex-col justify-end">
+                        <p class="text-gray-300 text-xs"><span class="text-cyan-500">Created at:</span>
+                            {{ $food->created_at->format('Y-m-d H:i') }}</p>
+                        <p class="text-gray-300 text-xs"><span class="text-cyan-500">Updated at:</span>
+                            {{ $food->updated_at->format('Y-m-d H:i') }}</p>
+                    </div>
+                    <div class="flex justify-end ">
+                        <div class="flex gap-1 items-center">
+                            <a class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm sm:px-5 sm:py-2.5 py-1 px-2 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
+                                href={{ route('foods.edit', $food->id) }}>Edit</a>
+                            <form action="{{ route('foods.destroy', $food->id) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit"
+                                    class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm sm:px-5 sm:py-2.5 py-1 px-2 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">Delete</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
 
 
-
-
-            <div class="max-w-7xl mx-auto space-y-5">
-                <div class="flex justify-between items-center border-b border-gray-700">
-                    <h1 id={{ $food->id }} class="text-white">{{ ucfirst($food->title) }}</h1>
-                    <div class="flex gap-1 items-center">
-                        <a class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm sm:px-5 sm:py-2.5 py-1 px-2 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
-                            href={{ route('role.edit', $food->id) }}>Edit</a>
-                        <form action="{{ route('role.destroy', $food->id) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit"
-                                class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm sm:px-5 sm:py-2.5 py-1 px-2 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">Delete</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-
-            <div class="flex flex-col justify-end">
-                <p class="text-gray-300 text-xs"><span class="text-cyan-500">Created at:</span>
-                    {{ $food->created_at->format('Y-m-d H:i') }}</p>
-                <p class="text-gray-300 text-xs"><span class="text-cyan-500">Updated at:</span>
-                    {{ $food->updated_at->format('Y-m-d H:i') }}</p>
-            </div>
-
-            <div class="flex justify-end">
-                <div class="flex gap-1 items-center">
-                    <a class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm sm:px-5 sm:py-2.5 py-1 px-2 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
-                        href={{ route('role.edit', $food->id) }}>Edit</a>
-                    <form action="{{ route('role.destroy', $food->id) }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit"
-                            class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm sm:px-5 sm:py-2.5 py-1 px-2 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">Delete</button>
-                    </form>
-                </div>
-            </div>
         </div>
     </div>
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
         integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -38,6 +39,8 @@
             {{ $slot }}
         </main>
     </div>
+
 </body>
+<script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.umd.js"></script>
 
 </html>
